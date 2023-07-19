e56bbe6245ada1e7247afbd98727bccc00555492
stp4 <w.peter@statistik-peter.at>
2023-07-19 09:53:55 +0200

    Anova für lm Objekte

--- R/Experimentelle-funktionen.R
+++ R/Experimentelle-funktionen.R
@@ -12,7 +12,7 @@
 #' @param p.adjust.method method for adjusting p value c("holm", "hochberg", "hommel", "bonferroni", "BH", "BY",
 #   "fdr", "none")
 #' @param alternative,mu,paired,exact,correct,conf.int,conf.level,tol.root,digits.rank not used
-#'
+#' @export
 #' @return data.frame
 #'
 #' @examples
@@ -89,3 +89,58 @@ Post_Hoc <- function(...,
 #  summary(lm(res_aov))
 
 
+
+
+#' Anova
+#'
+#'  Anova from car::Anova
+#'
+#' @param ...  lm-Objects
+#' @param names  Response
+#' @param include.eta an tbll_extract
+#' @param type an car::Anova
+#'
+#' @return tibble, data.frame
+#' @export
+#'
+#' @examples
+#'
+#' ctl <- c(4.17,5.58,5.18,6.11,4.50,4.61,5.17,4.53,5.33,5.14)
+#' trt <- c(4.81,4.17,4.41,3.59,5.87,3.83,6.03,4.89,4.32,4.69)
+#' group <- gl(2, 10, 20, labels = c("Ctl","Trt"))
+#' weight <- c(ctl, trt)
+#' lm.D9 <- lm(weight ~ group)
+#' lm.D90 <- lm(weight ~ group - 1) # omitting intercept
+#'
+#'
+#' Tbll_anova(lm.D9, lm.D90)
+#'
+#'
+Tbll_anova <- function(...,
+                       names = NULL,
+                       include.eta = TRUE,
+                       type=c("II","III", 2, 3)) {
+  dots <- list(...)
+
+  if (is.null(names)) {
+    names <-
+      gsub("[~??+\\:=]", "",
+           as.character(as.list(sys.call()))[seq_len(length(dots)) + 1])
+  }
+
+  if (length(dots) == 1)
+    tbll_extract(car::Anova(dots[[1]], type=type),
+                 include.eta = include.eta)
+  else{
+    rslt <-  NULL
+    for (i in  seq_along(dots)) {
+      rst <-   cbind(Response = "",
+                     tbll_extract(car::Anova(dots[[i]], type = type),
+                                  include.eta = include.eta))
+      rst[1, 1] <- names[i]
+      rslt <-  rbind(rslt, rst)
+
+    }
+    rslt
+  }
+}
