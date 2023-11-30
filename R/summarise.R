adc6db03cfdc62a4630bcc6e948cc0991d7d54fe
stp4 <w.peter@statistik-peter.at>
2023-11-30 09:46:09 +0100

    Fehler mit NA in mit tibble behoben.

--- R/summarise.R
+++ R/summarise.R
@@ -151,6 +151,9 @@ Summarise <- function(...,
                                 names_from = !!rhs,
                                 values_from = values_from)
   }
+ # print(head(rslts))
+  if(any(is.na(names(rslts))))
+  names(rslts)[is.na(names(rslts))] <- "NA"
   tibble::as_tibble(rslts)
 }
 
