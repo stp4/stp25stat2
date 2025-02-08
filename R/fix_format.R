67b43be01475c49bc8e90a480ada0d1fc20fc69d
Wolfgang Peter <w.peter@statistik-peter.at>
2025-02-08 07:02:09 +0100

    vereinfacht

--- R/fix_format.R
+++ R/fix_format.R
@@ -1,26 +1,59 @@
 
 #' fix_format
 #'
-#' @param ... all
+#' @description
+#' Formatieren der Zahlen für die Ausgabe mit Output()
 #'
-#' @returns data.frame
+#' fix_format():
+#' Alle numeric werden entweder geraten oder mit einer fixen
+#' Nachkomastelle formatiert.
+#'
+#' fix_format2(): Erratet die p-Werte und formatiert
+#'
+#'
+#' @param x an R object. data.frame or list
+#' @param digits integer. if Null stp25stat2:::format_guess()
+#'  a positive integer indicating how many significant digits are to be used
+#' @param names_repair logical. P-value and SE
+#' @param include.rownames logical. TRUE
+#' @param p.value character.  pattern for p_values
+#' @param se character. pattern for Estimate and SE
+#' @param df character. pattern for degree of fredom
+#'
 #' @export
+#'
+#' @importFrom stringr str_starts
+#' @importFrom purrr map2
+#' @importFrom dplyr bind_cols
+#' @importFrom stp25tools fix_to_tibble
+#'
+#' @returns A tbble data.frame the same length as the input.
+#'
 #'
 #' @examples
 #'
 #' df2 <-
-#' data.frame(
-#' term = c("A", "B", "C", "D"),
-#' Estimate = c(23.5, .14, 5.6, 2.9876),
-#' df1 = c(3.3, 35., 7.8, 2.1),
-#' df = c(3, 35, 7, 2),
-#' N = c(33, 35, 78, 21),
-#' F.value = c(2.73, 12.444, 14.576, 30.412),
-#' pvalue = c(0.73, 0.044, 0.056, 0.042),
-#' stringsAsFactors =FALSE
-#' )
+#'   data.frame(
+#'     term = c("A", "B", "C", "D"),
+#'     Estimate = c(23.5567, .148990, 5.643256, 2.9876),
+#'     df1 = c(3.3, 35., 7.8, 2.1),
+#'     df = c(3, 35, 7, 2),
+#'     N = c(33, 35, 78, 21),
+#'     F.value = c(2.73345, 12.4446, 14.5767, 30.4128),
+#'     pvalue = c(0.73123, 0.044456, 0.056789, 0.042654),
+#'     stringsAsFactors =FALSE
+#'   )
 #'
+#' # Erratet die p-Wert
 #' fix_format(df2)
+#'
+#' # versucht zu eraten wie viele digits
+#' fix_format2(df2)
+#'
+#' # fest vorgegeben
+#' fix_format2(df2, digits = 2)
+#'
+#'
 #'
 #'
 #' #  require(car)
@@ -34,20 +67,13 @@
 #' # summary(mod1)$coefficients |> fix_format()
 #' # Anova(mod2) |> fix_format()
 #' # summary(mod2)$coefficients |> fix_format()
-fix_format <- function(...) {
+#'
+fix_format <- function(x, ...) {
   UseMethod("fix_format")
 }
 
 
-#' @param x data.frame or list
-#'
-#' @param digits Nachkomastellen
-#' @param names_repair  P-value and SE
-#' @param include.rownames logical.
-#' @param p.value pattern
-#' @param se pattern
-#' @param df pattern
-#'
+
 #' @export
 fix_format.default <- function(x,
                                digits = NULL,
@@ -61,7 +87,9 @@ fix_format.default <- function(x,
                                se = c("Std\\. Error", "est.std"),
                                df = c("N", "Df")) {
   x <-
-    stp25tools::fix_to_tibble(x, include.rownames = include.rownames)
+    stp25tools::fix_to_tibble(
+      x,
+      include.rownames = include.rownames)
   data_names <- names(x)
   detect_pvalue <-
     stringr::str_starts(data_names, paste0(p.value, collapse = "|"))
@@ -78,10 +106,27 @@ fix_format.default <- function(x,
     digits <- ifelse(sapply(x, is.numeric), "numeric", NA)
     # digits[which(detect_se)] <- "numeric"
     digits[which(detect_pvalue)] <- "pvalue"
-    digits[which(detect_df)] <- "integer"
+    # digits[which(detect_df)] <- "integer"
   }
 
-  purrr::map2_dfc(x, digits,
+
+  # purrr::map2_dfc(x, digits,
+  #                 function(x, digits) {
+  #                   if (is.na(digits))
+  #                     x
+  #                   else if (is.numeric(digits))
+  #                     render_f(x, digits)
+  #                   else if (digits == "pvalue")
+  #                     rndr_P(x, include.symbol = FALSE)
+  #                   else if (digits == "numeric")
+  #                     render_f(x, 2)
+  #                   else
+  #                     render_f(x, 0)
+  #                 })
+  #
+
+
+  res <- purrr::map2(x, digits,
                   function(x, digits) {
                     if (is.na(digits))
                       x
@@ -90,14 +135,17 @@ fix_format.default <- function(x,
                     else if (digits == "pvalue")
                       rndr_P(x, include.symbol = FALSE)
                     else if (digits == "numeric")
-                      render_f(x, 2)
+                      render_f(x, NULL)
                     else
-                      render_f(x, 0)
+                      x #render_f(x, 0)
                   })
+
+  dplyr::bind_cols(res)
+
+
 }
 
 #' @export
-
 fix_format.list <-
   function(x, ...) {
     rslt <- NULL
@@ -115,8 +163,8 @@ fix_format.list <-
 
 
 
-# fixed digits
-#' @noRd
+#' @rdname fix_format
+#' @export
 fix_format2 <-  function(x,
                          digits = NULL,
                          include.rownames = TRUE) {
