ba57e0e9b95113cc7e4db9552539694a254b4fbb
Wolfgang Peter <w.peter@statistik-peter.at>
2025-06-11 09:50:00 +0200

    Dokumentation

--- R/tbll_reg.R
+++ R/tbll_reg.R
@@ -9,7 +9,6 @@
 #' AIC and  BIC  are for Selecting Regression Models
 #'
 #' @param ... ein oder mehrer Modelle
-#' @param include.label  Namen oder Labels FALSE
 #' @param names Spaltenname
 #' @param digits  Nachkomastellen
 #' @param include.b,include.ci  estimate
@@ -38,13 +37,27 @@
 #'   lm1, lm2,
 #'   include.p = FALSE,
 #'   include.ci = TRUE,
-#'   include.se=FALSE
+#'   include.se = FALSE
 #' )
 #'
 #'
+#'  Tbll_reg(lm1)
+#'
+#'
+#'
+#'  #' Alternative
+#'
+#'  #' parameters::model_parameters(lm1) |>
+#'  #' insight::format_table(select = "minimal")
+#'
+#'  #' library(modelsummary)
+#'  #' modelsummary(list(mod1=lm1, mod2=lm2), output = "gt") |>
+#'  #' Output("gt")
+#'
+#'
+#'
 Tbll_reg  <- function(...,
-                      include.label = FALSE,
-                      # geht nicht
+                     # include.label = FALSE,
                       names = NULL,
                       digits = NULL,
                       include.b = TRUE,
@@ -57,7 +70,6 @@ Tbll_reg  <- function(...,
                       conf.method = "Wald",  # c("Wald", "Z") bei glm
                       include.p = FALSE,
                       include.stars = if (include.p) FALSE  else TRUE,
-
                       include.param = TRUE,
                       include.gof = TRUE,
 
@@ -138,144 +150,175 @@ Tbll_reg  <- function(...,
 
 #' @rdname Tbll_reg
 #' @export
-Tbll_reg_long  <- function(...,
-                            include.label = FALSE,
-                            include.intercept = FALSE,
-                            # geht nicht
-                            #names = NULL,
-                            digits = NULL,
-                            include.b = TRUE,
-                            include.se = FALSE,
-                            include.beta = FALSE,
-                            include.ci = TRUE,
-                            include.odds = FALSE,
-                            include.odds.ci = FALSE,
-                            include.statistic = FALSE,
-                            conf.method = "Wald",
-                            # c("Wald", "Z") bei glm
-                            include.p = FALSE,
-                            include.stars = if (include.p)
-                              FALSE
-                            else
-                              TRUE,
+Tbll_reg_long <-
+  function(...,
+       #    include.label = FALSE,
+           include.intercept = FALSE,
+           names = NULL,
+           digits = NULL,
+           include.b = TRUE,
+           include.se = FALSE,
+           include.beta = FALSE,
+           include.ci = TRUE,
+           include.odds = FALSE,
+           include.odds.ci = FALSE,
+           include.statistic = FALSE,
+           conf.method = "Wald",  # c("Wald", "Z") bei glm
+           include.p = FALSE,
+           include.stars = if (include.p) FALSE else TRUE,
 
-                            include.r = TRUE,
-                            include.pseudo = FALSE,
-                            include.aic = FALSE,
-                            include.bic = include.aic,
-                            include.test = FALSE,
-                            include.devianze = FALSE,
-                            include.param = TRUE,
-                            include.rmse = FALSE,
-                            include.sigma = FALSE,
+           include.r = TRUE,
+           include.pseudo = FALSE,
+           include.aic = FALSE,
+           include.bic = include.aic,
+           include.test = FALSE,
+           include.devianze = FALSE,
+           include.param = TRUE,
+           include.rmse = FALSE,
+           include.sigma = FALSE,
+           include.obs = FALSE,
+           include.gof = include.r | include.pseudo | include.aic | include.bic | include.test,
 
-                            include.obs = TRUE,
-                            include.gof = include.r |
-                              include.pseudo |
-                              include.aic |
-                              include.bic | include.test,
+           include.loglik = FALSE,
+           include.minus.LL = include.loglik,
+           # include.ftest=TRUE,
+           # include.r=FALSE, #include.pseudo=FALSE,
+           # include.heteroskedasticity = TRUE,
+           # include.durbin = TRUE,
+           # include.levene = FALSE,
+           # include.bartlett = FALSE,
+           # include.vif=FALSE,
+           # include.sigma=FALSE,
+           # include.rmse=FALSE,
+           # include.aic = TRUE,
+           # include.bic = TRUE,
+           # include.residual=TRUE,
+           # include.normality=FALSE,
+           # include.multicollin=include.vif,
+           # include.deviance=TRUE,
+           include.custom = NULL) {
+    # models <- NULL
+    gofs <- NULL
+    rslt <- NULL
+    caption <- "Regression"
+    note <- ""
 
-                            #  include.ftest=TRUE,
-                            include.loglik = FALSE,
-                            include.minus.LL = include.loglik,
-                            #include.minus.LL=include.loglik,
-                            # include.r=FALSE, #include.pseudo=FALSE,
-                            # include.heteroskedasticity = TRUE,
-                            # include.durbin = TRUE,
-                            # include.levene = FALSE,
-                            # include.bartlett = FALSE,
-                            # include.vif=FALSE,
-                            # include.sigma=FALSE,
-                            # include.rmse=FALSE,
-                            # include.aic = TRUE,
-                            # include.bic = TRUE,
-                            #  include.residual=TRUE,
-                            #  include.normality=FALSE,
-                            #  include.multicollin=include.vif,
-                            #  include.deviance=TRUE,
-                            include.custom = NULL) {
-  fit <- list(...)
+    fit <- list(...)
 
-  coeff <-
-    stp25stat2:::regression_table(
-      fit,
-      include.param = include.param,
-      include.gof = include.gof,
-      include.custom = include.custom,
-      include.b = include.b,
-      include.se = include.se,
-      include.beta = include.beta,
-      include.ci = include.ci,
-      include.odds = include.odds,
-      include.odds.ci = include.odds.ci,
-      include.statistic = include.statistic,
-      include.p = include.p,
-      include.stars = include.stars,
-      include.df = FALSE,
-      include.effects = c("ran_pars", "fixed"),
-      ci.level = .95,
-      conf.method = conf.method,
-      digits = digits,
-      digits.param = 3,
-      digits.odds = 2,
-      digits.test = 2,
-      digits.beta = 2,
-      format = "fg",
+    if (inherits(fit[[1]], "rowwise_df")) {
+      if (length(fit) != 1)
+        warning(
+          "\n\nEtwas ist faul: es ist eine liste der Laenge = ",
+          length(fit),
+          " gekommen!\n",
+          "Ich erwarte hier ein nest_by Modell."
+        )
+      # hier kommt
+      # nest_by(cyl) |>
+      #  mutate(models = list(lm(mpg ~ hp, data)))
+      fit <- fit[[1]]
+      fit <- fit |> dplyr::pull(models, name = names(fit[1]))
 
-      dictionary = c(
-        std.error = "SE",
-        estimate = "b",
-        p.value = "p"
-      ),
-      return_list = TRUE
-    )
+      if (is.null(names)) {
+        names <- abbreviate(gsub("[~??+\\:=]", "", names(fit)), minlength = 7)
+      }
+    } else if (is.null(names)) {
+      names <- abbreviate(gsub("[~??+\\:=]", "", as.character(as.list(sys.call(
+
+      )))[seq_len(length(fit)) + 1]), minlength = 7)
+    }
+
+    if (inherits(fit[[1]], "coxph")) {
+      include.rmse <- FALSE
+    }
 
 
+    coeff <-
+      regression_table(
+        fit,
+        custom.model.names = names,
+        include.param = include.param,
+        include.gof = include.gof,
+        include.custom = include.custom,
+        include.b = include.b,
+        include.se = include.se,
+        include.beta = include.beta,
+        include.ci = include.ci,
+        include.odds = include.odds,
+        include.odds.ci = include.odds.ci,
+        include.statistic = include.statistic,
+        include.p = include.p,
+        include.stars = include.stars,
+        include.df = FALSE,
+        include.effects = c("ran_pars", "fixed"),
+        ci.level = .95,
+        conf.method = conf.method,
+        digits = digits,
+        digits.param = 3, digits.odds = 2, digits.test = 2, digits.beta = 2,
+        format = "fg",
 
-  gofs <- NULL
-  rslt <- NULL
-  for (i in seq_along(coeff)) {
-    x <- coeff[[i]]
-    if (!include.intercept)
-      x <- x[-1, ]
-
-    if (include.gof) {
-      gofs <- extract_gof(
-        fit[[i]],
-        include.ftest = include.test,
-        include.loglik = include.loglik,
-        include.minus.LL = include.minus.LL,
-        include.r = any(c(include.r, include.pseudo)),
-        include.heteroskedasticity = FALSE,
-        include.durbin = FALSE,
-        include.levene = FALSE,
-        include.bartlett = FALSE,
-        include.sigma = include.sigma,
-        include.rmse = include.rmse,
-        include.aic = include.aic,
-        include.bic = include.bic,
-        include.residual = FALSE,
-        include.normality = FALSE,
-        include.multicollin = FALSE,
-        include.deviance = include.devianze
+        dictionary = c(
+          std.error = "SE",
+          estimate = "b",
+          p.value = "p"
+        ),
+        return_list = TRUE
       )
 
-      gf <- t(gofs[2])
-      colnames(gf)  <-  gofs[[1]]
+    for (i in seq_along(coeff)) {
 
-      x <-
-        dplyr::bind_cols(x, rbind(gf, matrix(rep(
-          "", (nrow(x) - 1) * ncol(gf)
-        ), ncol = ncol(gf))))
+      x <- coeff[[i]]
+      if (!include.intercept)
+        x <- x[-1, ]
+
+      if (include.gof) {
+        gofs <- extract_gof(
+          fit[[i]],
+          include.ftest = include.test,
+          include.loglik = include.loglik,
+          include.minus.LL = include.minus.LL,
+          include.r = any(c(include.r, include.pseudo)),
+          include.heteroskedasticity = FALSE,
+          include.durbin = FALSE,
+          include.levene = FALSE,
+          include.bartlett = FALSE,
+          include.sigma = include.sigma,
+          include.rmse = include.rmse,
+          include.aic = include.aic,
+          include.bic = include.bic,
+          include.residual = FALSE,
+          include.normality = FALSE,
+          include.multicollin = FALSE,
+          include.deviance = include.devianze
+        )
+
+        if (!include.obs) {
+          gofs <- gofs[-which(gofs[[1]] == "Obs"), ]
+
+        }
+
+        gf <- t(gofs[2])
+        colnames(gf)  <-  gofs[[1]]
+
+        x <-
+          dplyr::bind_cols(x, rbind(gf, matrix(rep(
+            "", (nrow(x) - 1) * ncol(gf)
+          ), ncol = ncol(gf))))
+
+      }
+
+      nms <- c(names(coeff)[i], rep("", nrow(x) - 1))
+      coeff[[i]] <- dplyr::bind_cols("Model" = nms, x)
 
     }
 
-    nms <- c(names(coeff)[i], rep("", nrow(x) - 1))
-    coeff[[i]] <- dplyr::bind_cols("Model" = nms, x)
+    prepare_output(dplyr::bind_rows(coeff),
+                   caption,
+                   note,
+                   N = NULL,
+                   include.n = NULL)
 
   }
-  dplyr::bind_rows(coeff)
-}
 
 
 #' @rdname Tbll_reg
@@ -622,3 +665,33 @@ regression_table <-
 
     result
   }
+
+
+#' @rdname Tbll_reg
+#' @description
+#'  Tbll_model(): ist eine Copie von parameters::model_parameters und insight::format_table
+#'
+#' @export
+Tbll_model <- function(x,
+                       digits = 2,
+                       ci_digits = digits,
+                       p_digits = 3,
+                       select = "minimal",
+                       ...) {
+  dots <- list(...)
+  if( any( grepl( "include\\.",  names(dots) ))) {
+    print(names(dots))
+    stop( "\nDas geht so nicht 'include' habe ich nicht implementiert!\n Schau einfach in die Hilfe von '?insight::format_table' und\nerledige das mit 'select = ...'")
+  }
+  parameters::model_parameters(x) |>
+    insight::format_table(
+      digits = digits,
+      ci_digits = ci_digits,
+      p_digits = p_digits,
+      select = select
+    ) |>
+    prepare_output(
+      caption = paste("Regression Models Response: ", insight::find_response(x)),
+      note =""
+    )
+}
